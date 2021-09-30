2ae1ced834b175fdf7b099378d1de1ccfcd767f2
Jason Fong <jasonfong12398@gmail.com>
2021-09-30 17:11:33 -0400

    item loading

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 export default class Workspace extends React.Component {
+
     render() {
+        const currentList = this.props.currentList;
+        if(currentList == null){
         return (
             <div id="top5-workspace">
                 <div id="workspace-edit">
@@ -14,6 +17,29 @@ export default class Workspace extends React.Component {
                     </div>
                 </div>
             </div>
-        )
+        )}
+        else{
+            console.log(currentList.items[0]);
+            return(
+                <div id="top5-workspace">
+                <div id="workspace-edit">
+                    <div id="edit-numbering">
+                        <div className="item-number">1.</div>
+                        <div className="item-number">2.</div>
+                        <div className="item-number">3.</div>
+                        <div className="item-number">4.</div>
+                        <div className="item-number">5.</div>
+                    </div>
+                    <div id="edit-items">
+                        <div className="top5-item">{currentList.items[0]}</div>
+                        <div className="top5-item">{currentList.items[1]}</div>
+                        <div className="top5-item">{currentList.items[2]}</div>
+                        <div className="top5-item">{currentList.items[3]}</div>
+                        <div className="top5-item">{currentList.items[4]}</div>
+                    </div>
+                </div>
+            </div>
+            )
+        }
     }
 }
